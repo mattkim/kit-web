a59614ce014c82d9960e038cb86a98b1629fdea2
Matt Kim <matthkim@gmail.com>
2016-11-07 02:58:16 -0800

    Adding redux basic integration with react

--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router'
 import App from './components/App/App';
 import Event from './components/Event/Event';
+import { createStore } from 'redux'
+import reducer from './reducers/index'
 import './index.css';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer)
+
+// Dynamically wraps store and component into Provider.
+function Provide(store, component) {
+  return React.createClass({
+    render() {
+      return (
+        <Provider store={store}>
+          {component}
+        </Provider>
+      )
+    }
+  })
+}
 
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="event" component={Event}/>
+    <Route path="/" component={Provide(store, <App/>)}>
+      <Route path="event" component={Provide(store, <Event/>)}/>
     </Route>
   </Router>
 ), document.getElementById('root'))
