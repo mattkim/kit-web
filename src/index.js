d7b3d28d74e5fe56bbf3931d51fb6f07297fdfa3
Matt Kim <matthkim@gmail.com>
2016-11-07 01:33:44 -0800

    Adding react-router + empty event component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, browserHistory } from 'react-router'
+import App from './components/App/App';
+import Event from './components/Event/Event';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="event" component={Event}/>
+    </Route>
+  </Router>
+), document.getElementById('root'))
